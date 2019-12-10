43e306c2577723970d3b098d3675980dcc3a3c2e
Bárbara <bazinha_ap@hotmail.com>
2019-12-10 17:28:20 -0300

    ajustes

--- src/app/pages/servico/servico.page.ts
+++ src/app/pages/servico/servico.page.ts
@@ -10,34 +10,8 @@ import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 })
 export class ServicoPage implements OnInit {
 
-  map: Map;
 
-  ionViewDidEnter() { this.leafletMap(); }
-
-  leafletMap() {
-    // In setView add latLng and zoom
-    this.map = new Map('mapId').setView([28.644800, 77.216721], 10);
-    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'edupala.com © ionic LeafLet',
-    }).addTo(this.map);
-
-
-    marker([28.6, 77]).addTo(this.map)
-      .bindPopup('Ionic 4 <br> Leaflet.')
-      .openPopup();
-
-    
-  }
-
-  /** Remove map when we have multiple map object */
-  ionViewWillLeave() {
-    this.map.remove();
-  }
-
-
-
-
-  [x: string]: any;
+   [x: string]: any;
   public servicoData: Servico = {};
 
   constructor(
